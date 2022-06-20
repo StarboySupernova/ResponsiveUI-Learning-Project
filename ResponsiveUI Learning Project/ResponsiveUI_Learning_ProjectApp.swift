b98be01bbc7dc4bcbd1eb7d8ee8a9371960b8131
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-06-21 00:09:36 +0100

    Initial Commit

---
+++ ResponsiveUI Learning Project/ResponsiveUI_Learning_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUI_Learning_ProjectApp.swift
+//  ResponsiveUI Learning Project
+//
+//  Created by user217235 on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUI_Learning_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
